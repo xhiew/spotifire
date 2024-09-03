6302f9054cd903a18aab27d02ef9b02bbd6c00b9
xhieu <hieunx1@vnpay.vn>
2024-09-03 16:54:19 +0700

    add gradient view

---
+++ Spotifire/Spotifire/Shared/CustomView/GradientView/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Spotifire
+//
+//  Created by xhieu on 2/9/24.
+//
+
+import UIKit
+
+final class GradientView: UIView {
+    override class var layerClass: AnyClass { return CAGradientLayer.self }
+    
+    @IBInspectable
+    var startColor: UIColor = .baseWhite {
+        didSet {
+            colors = (startColor, endColor)
+        }
+    }
+    
+    @IBInspectable
+    var endColor: UIColor = .baseWhite {
+        didSet {
+            colors = (startColor, endColor)
+        }
+    }
+    
+    var colors: (start: UIColor, end: UIColor)? {
+        didSet { updateLayer() }
+    }
+    
+    private func updateLayer() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = colors.map { [$0.start.cgColor, $0.end.cgColor] }
+    }
+}
