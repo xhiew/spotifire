f6ccb67938b839b1cd6f95bb77f02e2522bed38e
xhieu <hieunx1@vnpay.vn>
2024-11-07 09:26:31 +0700

    add ViewController

--- Spotifire/Spotifire/Shared/Base/ViewController/ViewController.swift
+++ Spotifire/Spotifire/Shared/Base/ViewController/ViewController.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Lifecycle
+    init() { super.init(nibName: Self.aka, bundle: .main) }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        print("deinit \(self)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 }
